e8022a5bb538a78aa434d19214d6b6ecfcf14793
OJ Kwon <kwon.ohjoong@gmail.com>
2018-01-23 11:18:26 -0800

    docs(example): update example for multidict

--- example/browserWindow.ts
+++ example/browserWindow.ts
@@ -5,16 +5,25 @@ enableLogger(console);
 
 const init = async () => {
   const browserWindowProvider = new SpellCheckerProvider();
+  browserWindowProvider.verboseLog = true;
+
   (window as any).browserWindowProvider = browserWindowProvider;
   await browserWindowProvider.initialize();
 
   await browserWindowProvider.loadDictionary(
-    'en',
+    'en-us',
     path.join(path.resolve('./'), 'en-US.dic'),
     path.join(path.resolve('./'), 'en-US.aff')
   );
 
-  browserWindowProvider.switchDictionary('en');
+  await browserWindowProvider.loadDictionary(
+    'en-gb',
+    path.join(path.resolve('./'), 'en-gb.dic'),
+    path.join(path.resolve('./'), 'en-gb.aff')
+  );
+
+  //run spell check against en-US and en-GB both, while en-US is primary dictionary
+  browserWindowProvider.switchDictionary('en-us', true);
 };
 
 init();
